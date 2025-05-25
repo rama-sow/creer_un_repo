4c47b5687caf5eef212523c09629b9869ac939c6
rama-sow <rama.sow1010@gmail.com>
2025-05-25 16:39:37 +0200

    Créer greetings.py
    Signed-off-by: rama-sow <rama.sow1010@gmail.com>

---
+++ greetings.py
@@ -0,0 +1,3 @@
+print("Je suis entrain d'apprende Git et Github et j'adore ça ! ")
+nom = input("quel est ton nom?")
+print(f"Enchanté, {nom}! Passe une bonne journée en codant avec Git et Github.")
